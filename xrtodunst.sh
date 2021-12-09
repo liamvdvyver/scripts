b84378286c981090906720316babd5ea5f68ff11
Liam van der Vyver <1495063-liamvdvyver@users.noreply.gitlab.com>
2021-12-09 18:51:19 +1100

    feat: xrtodunstsh change frame_border to light bg

--- xrtodunst.sh
+++ xrtodunst.sh
@@ -4,25 +4,27 @@
 conf=~/.config/dunst/dunstrc
 
 bg=$(xrdb -query | grep 'background' | head -n1 | cut -f 2)
+b2=$(xrdb -query | grep 'color8' | head -n1 | cut -f 2)
 fg=$(xrdb -query | grep 'foreground' | head -n1 | cut -f 2)
-fc=$(xrdb -query | grep 'cursorColor' | cut -f 2)
+cc=$(xrdb -query | grep 'cursorColor' | cut -f 2)
 
 bgline="    background = \"$bg\""
+b2line="    frame_color = \"$b2\""
 fgline="    foreground = \"$fg\""
-fcline="    frame_color = \"$fc\""
+ccline="    frame_color = \"$cc\""
 
 echo "[urgency_low]" > $conf
 echo $bgline >> $conf
 echo $fgline >> $conf
-echo $fcline >> $conf
+echo $ccline >> $conf
 echo "" >> $conf
 echo "[urgency_normal]" >> $conf
 echo $bgline >> $conf
 echo $fgline >> $conf
-echo $fcline >> $conf
+echo $b2line >> $conf
 echo "" >> $conf
 echo "[urgency_critical]" >> $conf
 echo $bgline >> $conf
 echo $fgline >> $conf
-echo $fcline >> $conf
+echo $b2line >> $conf
 echo "" >> $conf
