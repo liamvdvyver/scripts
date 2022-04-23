9d671c3c1ccd83facb260ed204e60998aae54c15
Liam van der Vyver <1495063-liamvdvyver@users.noreply.gitlab.com>
2022-04-23 18:42:22 +1000

    feat: theme.sh reload kitty and nvim colours

--- theme.sh
+++ theme.sh
@@ -1,22 +1,30 @@
 #!/bin/sh
 # set Xresources and vim colours from positional arguments and update kitty and dunst configs from .Xresources
-theme="$1"
-vimtheme=$1$2
+
+# REQUIRED/SUPPORTED PROGRAMS
+
+# .Xresources with colorschemes
+# kitty, dusnt, nitrogen with config files to be edited
+# reloads i3wm, kitty, and nvim as server
+
+theme=${1-gruv}
 
 # set .Xresources theme
 
 xres=~/.Xresources
+xtheme=$theme
 
-sed -i "s/\(#define c[0-9]\{2\} \)[a-z]*\([0-9]\{2\}\)/\1$theme\2/" $xres
-sed -i "s/\(#define \)\([a-z]\{2\}2\?\)\( *\)[a-z]*\2/\1\2\3$theme\2/g" $xres
+sed -i "s/^\(#define c\)\([0-9]\{2\}\) [a-z]\+\2$/\1\2 $xtheme\2/" $xres
+sed -i "s/^\(#define \)\([a-z]\{2\}[0-9]\?\)\s[a-z]\+\2$/\1\2 $xtheme\2/" $xres
 
 xrdb $xres
 
 # set vim colourscheme
 
 vimconf=~/.config/nvim/init.vim
+vimtheme=$(cat $xres | grep "^\! $theme [a-z0-9]\+ \!$" | cut -s -d ' ' -f 3)
 
-sed -i "s/\(colorscheme \).*/\1$vimtheme/" $vimconf
+sed -i "s/^\(colorscheme \)[a-z0-9]*$/\1$vimtheme/" $vimconf
 
 # copy colours in .Xresources to kitty.conf
 
@@ -26,34 +34,52 @@ sed -i "/# FOLLOWING LINES WILL BE AUTOMATICALLY OVERWRITTEN/q" $kittyconf
 
 echo "" >> $kittyconf
 
-xrdb -query | grep ^* | sed -E "/ground2:|accent/d" | cut -c 2- | sed "s/://" | sed -r "s/^cursorColor/cursor/" >> $kittyconf
+kittyregex='^\*\(color[0-9]\{1,2\}\|background\|foreground\|cursorColor\):\s#[a-zA-Z0-9]\{6\}$'
+
+xrdb -query | grep "$kittyregex" |cut -c 2- | sed "s/:\s/ /" | sed -r "s/^cursorColor/cursor/" >> $kittyconf
 
 # copy colours in .Xresources to dunstrc
 
 dunstconf=~/.config/dunst/dunstrc
 
-bg=$(xrdb -query | grep '^\*background:' | cut -f 2)
-b2=$(xrdb -query | grep '^\*background2:' | cut -f 2)
-fg=$(xrdb -query | grep '^\*foreground:' | cut -f 2)
-ac=$(xrdb -query | grep '^\*accent:' | cut -f 2)
+bg=$(xrdb -query | grep '^\*background:\s#[a-zA-Z0-9]\{6\}$' | cut -f 2)
+fg=$(xrdb -query | grep '^\*foreground:\s#[a-zA-Z0-9]\{6\}$' | cut -f 2)
+wi=$(xrdb -query | grep '^\*windowi:\s#[a-zA-Z0-9]\{6\}$' | cut -f 2)
+wf=$(xrdb -query | grep '^\*windowf:\s#[a-zA-Z0-9]\{6\}$' | cut -f 2)
+wu=$(xrdb -query | grep '^\*windowu:\s#[a-zA-Z0-9]\{6\}$' | cut -f 2)
 
 bgline="    background = \"$bg\""
-b2line="    frame_color = \"$b2\""
 fgline="    foreground = \"$fg\""
-f2line="    frame_color = \"$fg\""
-acline="    frame_color = \"$ac\""
+wiline="    frame_color = \"$wi\""
+wfline="    frame_color = \"$wf\""
+wuline="    frame_color = \"$wu\""
 
-echo "[urgency_low]" > $dunstconf
+sed -i "/# FOLLOWING LINES WILL BE AUTOMATICALLY OVERWRITTEN/q" $dunstconf
+
+echo "" >> $dunstconf
+echo "[urgency_low]" >> $dunstconf
 echo $bgline >> $dunstconf
 echo $fgline >> $dunstconf
-echo $b2line >> $dunstconf
+echo $wiline >> $dunstconf
 echo "" >> $dunstconf
 echo "[urgency_normal]" >> $dunstconf
 echo $bgline >> $dunstconf
 echo $fgline >> $dunstconf
-echo $f2line >> $dunstconf
+echo $wfline >> $dunstconf
 echo "" >> $dunstconf
 echo "[urgency_critical]" >> $dunstconf
 echo $bgline >> $dunstconf
 echo $fgline >> $dunstconf
-echo $acline >> $dunstconf
+echo $wuline >> $dunstconf
+
+# set nitrogen wallpaper
+
+nitrogenconf=~/.config/nitrogen/bg-saved.cfg 
+
+sed -i "s/^\(file=\/home\/lvdv\/drive\/Pictures\/Wallpapers\/themes\/\)[a-zA-Z0-9]\+\(\.jpg\)/\1$theme\2/" $nitrogenconf
+
+# reload things
+
+i3-msg restart
+pgrep kitty | xargs kill -s USR1
+nvim --server $NVIM_LISTEN_ADDRESS --remote-send ':source $MYVIMRC<CR>'
