487b02beae6d43d5a977e4ec8e76c4df57366f41
Liam van der Vyver <1495063-liamvdvyver@users.noreply.gitlab.com>
2022-04-24 20:53:32 +1000

    feat: add usermenu.sh

---
+++ usermenu.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Credit to github.com/stefanv
+
+shopt -s nullglob globstar
+
+typeit=0
+if [[ $1 == "--type" ]]; then
+    typeit=1
+    shift
+fi
+
+prefix=${PASSWORD_STORE_DIR-~/.password-store}
+password=$(cd $prefix && find * -type f -iname "*.gpg" -exec sh -c 'printf "%s\n" "${@%.gpg}"' _ {} + | dmenu -i "$@")
+
+[[ -n $password ]] || exit
+
+if [[ $typeit -eq 0 ]]; then
+    username=$(pass show "$password" | grep -o -P "user(name)?: \K.*")
+    echo "$username" | xclip  # send the username to middle button paste
+else
+    username=$(pass show "$password" | grep -o -P "user(name)?: \K.*")
+    xdotool type $username # type the username
+fi
