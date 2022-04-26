4feb3b1480dbb7728a182756e8a4a6a21178c5e1
Liam van der Vyver <1495063-liamvdvyver@users.noreply.gitlab.com>
2022-04-26 20:23:05 +1000

    fix: usermenu.sh quote variables for splitting

--- usermenu.sh
+++ usermenu.sh
@@ -10,7 +10,7 @@ if [[ $1 == "--type" ]]; then
 fi
 
 prefix=${PASSWORD_STORE_DIR-~/.password-store}
-password=$(cd $prefix && find * -type f -iname "*.gpg" -exec sh -c 'printf "%s\n" "${@%.gpg}"' _ {} + | dmenu -i "$@")
+password=$(cd "$prefix" && find * -type f -iname "*.gpg" -exec sh -c 'printf "%s\n" "${@%.gpg}"' _ {} + | dmenu -i "$@")
 
 [[ -n $password ]] || exit
 
@@ -19,5 +19,5 @@ if [[ $typeit -eq 0 ]]; then
     echo "$username" | xclip  # send the username to middle button paste
 else
     username=$(pass show "$password" | grep -o -P "user(name)?: \K.*")
-    xdotool type $username # type the username
+    xdotool type "$username" # type the username
 fi
