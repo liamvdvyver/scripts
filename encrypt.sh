d155428635c2b87c1552876f03c2d1d00261a5b3
Liam van der Vyver <1495063-liamvdvyver@users.noreply.gitlab.com>
2022-04-27 11:06:53 +1000

    feat: add encrypt.sh

---
+++ encrypt.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Encrypt, commit and push dotfiles with yadm
+
+yadm encrypt
+cp ~/.local/share/yadm/archive ~/git/dotfiles-secret || exit
+cd ~/git/dotfiles-secret || exit
+git add archive
+git commit -am "Encrypt files"
+git push
