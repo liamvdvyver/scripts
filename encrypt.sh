9cf2ff13cb806ae229740ee6d79894542d7c502a
Liam van der Vyver <1495063-liamvdvyver@users.noreply.gitlab.com>
2022-05-11 02:26:08 +1000

    feat: encrypt.sh add --decrypt functionality

--- encrypt.sh
+++ encrypt.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 # Encrypt, commit and push dotfiles with yadm
 
-yadm encrypt
-cp ~/.local/share/yadm/archive ~/git/dotfiles-secret || exit
-cd ~/git/dotfiles-secret || exit
-git add archive
-git commit -am "chore: encrypt files"
-git push
+gitdir=~/git/dotfiles-secret
+yadmdir=~/.local/share/yadm
+
+if [ -z "$1" ]; then
+    yadm encrypt
+    cp $yadmdir/archive $gitdir || exit
+    cd $gitdir || exit
+    git add archive
+    git commit -am "chore: encrypt files"
+    git push
+elif [ "$1" = "-d" ] || [ "$1" = "--decrypt" ]; then
+    cd $gitdir || exit
+    git pull
+    cp archive $yadmdir || exit
+    yadm decrypt
+else
+    echo "usage: encrypt.sh [--decrypt]"
+    exit
+fi
