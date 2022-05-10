ec1a9fe4b77c75ffcebc40004e2723dfe66c6099
Liam van der Vyver <1495063-liamvdvyver@users.noreply.gitlab.com>
2022-05-11 02:27:20 +1000

    feat: rice.sh add zathura support

--- rice.sh
+++ rice.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
-
-# Copies theme configuration from .Xresources to neovim, kitty, dunst, i3wm, and
-# sets corresponding wallpaper with nitrogen
-#
-# NOTE: when specifying the layout of config files, ^ and $ are used to denote
-# the start and end of lines
+# Source Xresources and copy configuration to config files
+# Themes and documentation at gitlab.com/liamvdvyver/dotfiles
 
 # INITAL VARIABLES --------------------------------------------------------- {{{
 
 xres=~/.Xresources  # location of Xresources config
-xdir=~/.config/xres/ # location of theme files
+xdir=~/.config/rice/themes/ # location of theme files
 
 if [ ! -f $xres ]; then # warn if not found (required for script)
     echo "no Xresources configuration found at $xres"
@@ -126,7 +122,6 @@ icondark=$(xrdb -query | grep 'rice.icon.dark:\s.\+$' | cut -f 2)
 iconlight=$(xrdb -query | grep 'rice.icon.light:\s.\+$' | cut -f 2)
 
 # Set single widget and icon theme based on $bg
-
 if [ "$bg" = "dark" ]; then
     gtk="$gtkdark"
     icon="$icondark"
@@ -144,14 +139,14 @@ gtk3conf=~/.config/gtk-3.0/settings.ini
 
 # Set themes in config files
 if [ -f $gtk2conf ]; then
-    sed -i "s/^\(gtk-theme-name=\)\"[a-zA-Z\-]\+\"$/\1\"$gtk\"/" $gtk2conf && \
-    sed -i "s/^\(gtk-icon-theme-name=\)\"[a-zA-Z\-]\+\"$/\1\"$icon\"/" $gtk2conf && \
+    sed -i "s/^\(gtk-theme-name=\)\"[a-zA-Z\-]*\"$/\1\"$gtk\"/" $gtk2conf && \
+    sed -i "s/^\(gtk-icon-theme-name=\)\"[a-zA-Z\-]*\"$/\1\"$icon\"/" $gtk2conf && \
     echo "updated $gtk2conf"
 fi
 
 if [ -f $gtk3conf ]; then
-    sed -i "s/^\(gtk-theme-name=\)[a-zA-Z\-]\+$/\1$gtk/" $gtk3conf && \
-    sed -i "s/^\(gtk-icon-theme-name=\)[a-zA-Z\-]\+$/\1$icon/" $gtk3conf && \
+    sed -i "s/^\(gtk-theme-name=\)[a-zA-Z\-]*$/\1$gtk/" $gtk3conf && \
+    sed -i "s/^\(gtk-icon-theme-name=\)[a-zA-Z]*$/\1$icon/" $gtk3conf && \
     echo "updated $gtk3conf"
 fi
 
@@ -185,7 +180,6 @@ kittyconf=~/.config/kitty/kitty.conf # set location of kitty config
 
 # Regex to match *colorDD, *background, *foregound, *cursorColor from Xresources
 # database:
-
 kittyregex='^\*\(color[0-9]\{1,2\}\|background\|foreground\|cursorColor\):\s#[a-zA-Z0-9]\{6\}$'
 
 # If the $warningline line is matched, delete all following lines and replace
@@ -193,12 +187,35 @@ kittyregex='^\*\(color[0-9]\{1,2\}\|background\|foreground\|cursorColor\):\s#[a-
 # formatting as follows:
 #
 # ^*cursorColor: #XXXXXX$ --> ^cursor #XXXXXX$
-
 if [ -f $kittyconf ] && grep -q "^$warningline$" $kittyconf; then
     sed -i "/^$warningline$/q" $kittyconf
     echo "" >> $kittyconf
     xrdb -query | grep "$kittyregex" | cut -c 2- | sed "s/:\s/ /" | sed -r "s/^cursorColor/cursor/" >> $kittyconf
     echo "updated $kittyconf"
+fi
+
+# }}}
+
+# SET ZATHURA COLOURS ------------------------------------------------------ {{{
+
+zathuraconf=~/.config/zathura/zathurarc
+
+zathbg=$(xrdb -query | grep '^\*background:\s#[a-zA-Z0-9]\{6\}$' | cut -f 2)
+zathfg=$(xrdb -query | grep '^\*foreground:\s#[a-zA-Z0-9]\{6\}$' | cut -f 2)
+zathbg2=$(xrdb -query | grep '^rice\.background\.active:\s#[a-zA-Z0-9]\{6\}$' | cut -f 2)
+zathfg2=$(xrdb -query | grep '^rice\.foreground\.active:\s#[a-zA-Z0-9]\{6\}$' | cut -f 2)
+zathbg3=$(xrdb -query | grep '^rice\.background\.focused:\s#[a-zA-Z0-9]\{6\}$' | cut -f 2)
+zathfg3=$(xrdb -query | grep '^rice\.foreground\.focused:\s#[a-zA-Z0-9]\{6\}$' | cut -f 2)
+zathbgr=$(xrdb -query | grep '^rice\*urgent:\s#[a-zA-Z0-9]\{6\}$' | cut -f 2)
+
+if [ -f "$zathuraconf" ]; then
+    sed -i "s/\(set [a-z\-]\+ \\\\\)\#[a-zA-Z0-9]\{6\}\( #fg\)/\1$zathfg\2/" "$zathuraconf"
+    sed -i "s/\(set [a-z\-]\+ \\\\\)\#[a-zA-Z0-9]\{6\}\( #bg\)/\1$zathbg\2/" "$zathuraconf"
+    sed -i "s/\(set [a-z\-]\+ \\\\\)\#[a-zA-Z0-9]\{6\}\( #bg2\)/\1$zathbg2\2/" "$zathuraconf"
+    sed -i "s/\(set [a-z\-]\+ \\\\\)\#[a-zA-Z0-9]\{6\}\( #fg2\)/\1$zathfg2\2/" "$zathuraconf"
+    sed -i "s/\(set [a-z\-]\+ \\\\\)\#[a-zA-Z0-9]\{6\}\( #bg3\)/\1$zathbg3\2/" "$zathuraconf"
+    sed -i "s/\(set [a-z\-]\+ \\\\\)\#[a-zA-Z0-9]\{6\}\( #fg3\)/\1$zathfg3\2/" "$zathuraconf"
+    sed -i "s/\(set [a-z\-]\+ \\\\\)\#[a-zA-Z0-9]\{6\}\( #bgr\)/\1$zathbgr\2/" "$zathuraconf"
 fi
 
 # }}}
@@ -221,13 +238,12 @@ dunstflline="frame_color = \"$dunstfl\""
 dunstfnline="frame_color = \"$dunstfn\""
 dunstfcline="frame_color = \"$dunstfc\""
 
-# Set dunsticonline from $icon and enable recursive lookup
+# Set $dunsticonline from $icon and enable recursive lookup:
 dunsticonline="icon_theme = \"$icon\""
 dunstlookline="enable_recursive_icon_lookup = \"true\""
 
 # If the $warningline line is matched, delete all following lines and replace
 # with $dunstXXline in appopriate sections:
-
 if [ -f $dunstconf ] && grep -q "^$warningline$" $dunstconf; then
     sed -i "/^$warningline$/q" $dunstconf
     {
@@ -260,14 +276,14 @@ fi
 
 # Set niteogen config location and regex for wallpaper directory:
 nitrogenconf=~/.config/nitrogen/bg-saved.cfg
-papedir='/home/lvdv/drive/Pictures/Wallpapers/themes/'
+papedir=~/.config/rice/papes/
 papedirregex="$(echo $papedir | sed 's/\//\\\//g')"
+wallpaper="$(ls $papedir -w 1 | grep "^$theme\.[a-zA_Z]\+$")"
 
 # If config is found and file exists names $theme, replace old theme.jpg with
 # current $theme.jpg in config:
-
-if [ -f $nitrogenconf ] && [ -f "$papedir$theme.jpg" ]; then
-    sed -i "s/^\(file=$papedirregex\).\+\(\.jpg\)/\1$theme\2/" $nitrogenconf && \
+if [ -f $nitrogenconf ] && [ "$wallpaper" ]; then
+    sed -i "s/^\(file=\).\+/\1$papedirregex$wallpaper/" $nitrogenconf && \
     echo "updated $nitrogenconf"
 fi
 
@@ -275,8 +291,7 @@ fi
 
 # RELOAD ------------------------------------------------------------------- {{{
 
-# Restart i3
-
+# Restart i3:
 if [ "$(command -v i3-msg)" ]; then i3-msg restart && echo "reloaded i3"; fi # restart i3 if installed
 
 # Send SIGUSR1 to runnin kitty instances (reloads config):
@@ -284,7 +299,10 @@ pgrep kitty | xargs kill -s USR1 && echo "reloaded kitty"
 
 # Find nvim server sockets and remote send command to reload config:
 nvim_socks="$(find /tmp/ -regex "/tmp/nvim[a-zA-Z0-9]+/0" -print 2>/dev/null | paste -s -d ' ')"
-echo "$nvim_socks" | xargs -r -n 1 nvim --remote-send ":source \$MYVIMRC<CR>" --server && \
+echo "$nvim_socks" | xargs -r -n 1 nvim --remote-send "<esc>:source \$MYVIMRC<CR>" --server && \
 echo "reloaded nvim"
+
+# Find zathura instances and source condig with dbus
+pgrep zathura | xargs -I '{}' dbus-send --type=method_call --dest=org.pwmt.zathura.PID-'{}' /org/pwmt/zathura org.pwmt.zathura.ExecuteCommand string:source
 
 # }}}
