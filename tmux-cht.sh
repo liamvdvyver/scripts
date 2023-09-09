cc22f6c98dc03fef7115d3e70cafa61b0eb572f0
Liam van der Vyver <91105007+liamvdvyver@users.noreply.github.com>
2023-09-09 17:07:11 +1000

    feat: tmux-cht.sh support man and tldr

--- tmux-cht.sh
+++ tmux-cht.sh
@@ -1,19 +1,48 @@
 #!/usr/bin/env bash
 # taken from github.com/ThePrimeagen/.dotfiles
+# adapted to provide man or tldr results
 
-langfile=~/.config/tmux/.tmux-cht-languages
-cmdfile=~/.config/tmux/.tmux-cht-commands
+if [[ $1 == "man" ]]; then
 
-selected=`cat $langfile $cmdfile | fzf`
-if [[ -z $selected ]]; then
-    exit 0
+    selected="$(man -k "" | cut -f 1 -d ' ' | fzf)"
+    if [[ -z $selected ]]; then
+        exit 0
+    fi
+
+    command="man $selected"
+
+elif [[ $1 == "tldr" ]]; then
+
+    read -p "Enter Command: " selected
+    if [[ -z $selected ]]; then
+        exit 0
+    fi
+
+    command="tldr $selected | less"
+
+else
+
+    langfile=~/.config/tmux/.tmux-cht-languages
+    cmdfile=~/.config/tmux/.tmux-cht-commands
+
+    selected=`cat $langfile $cmdfile | fzf`
+    if [[ -z $selected ]]; then
+        exit 0
+    fi
+
+    read -p "Enter Query: " query
+    query=`echo $query | tr ' ' '+'`
+
+    if grep -qs "$selected" "$langfile"; then
+        command="curl -s cht.sh\/$selected\/$query | less -R"
+    else
+        command="curl -s cht.sh\/$selected~$query | less -R"
+    fi
+
 fi
 
-read -p "Enter Query: " query
-
-if grep -qs "$selected" "$langfile"; then
-    query=`echo $query | tr ' ' '+'`
-    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+if [[ -n $TMUX ]]; then
+    tmux neww bash -c "$command"
 else
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | less -R"
+    eval "$command"
 fi
